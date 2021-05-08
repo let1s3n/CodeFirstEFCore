d30605def920b7e5b6da3fc8cff30c40c4b71a73
let1s3n <letisendev@gmail.com>
2021-05-08 10:29:14 -0500

    Agregar archivos de proyecto.

---
+++ S5_CodeFirstEFCore.Web/Models/SoccerPosition.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace S5_CodeFirstEFCore.Web.Models
+{
+    [Table("SoccerPosition")]
+    public class SoccerPosition
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        [Display(Name = "Position")]
+
+        public string Description { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        [Display(Name = "Descripcion")]
+
+        public string Code { get; set; }
+
+        public virtual List<Player> Player { get; set; }
+
+
+
+    }
+}
